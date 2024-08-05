56012be377bbd04fd0476a3d52e8591c3742d5d6
Raja Singh <rajasgh18@gmail.com>
2024-08-05 20:38:24 +0530

    added toaster in the main file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from "@/components/ui/sonner"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Toaster />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
